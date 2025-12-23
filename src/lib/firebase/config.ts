cf84e9bc199f7456efef2ff629ece1bc63f0ab09
Francisco CB <francb.movil@gmail.com>
2025-12-23 00:38:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,13 +1,20 @@
 // src/firebase/config.ts
 import { FirebaseOptions, getApp, getApps, initializeApp } from 'firebase/app';
 
-const firebaseConfig: FirebaseOptions = JSON.parse(
-  process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}'
-);
+const firebaseConfig: FirebaseOptions = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+};
 
 // Initialize Firebase
 let firebaseApp;
 if (!getApps().length) {
+  if (!firebaseConfig.apiKey) {
+    throw new Error('Missing Firebase config. Please check your .env file.');
+  }
   firebaseApp = initializeApp(firebaseConfig);
 } else {
   firebaseApp = getApp();
