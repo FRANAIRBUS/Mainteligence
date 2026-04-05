879872fc56ece298c47ae40e17d480c5e8aa1e43
Fran <francb.movil@gmail.com>
2026-04-05 15:26:44 +0100

    Actualiza posiciones panel

--- src/components/iot-panel-card.tsx
+++ src/components/iot-panel-card.tsx
@@ -2043,7 +2043,7 @@ function LegacyThermostatPanel({
             </div>
 
             <div
-              className="absolute right-[70.0%] top-[36.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
+              className="absolute right-[67.0%] top-[36.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
               style={inoutDigitalValueStyle}
             >
               <span className="inline-flex w-full items-end justify-end gap-[0.12em]">
@@ -2054,7 +2054,7 @@ function LegacyThermostatPanel({
               </span>
             </div>
             <div
-              className="absolute right-[70.0%] top-[62.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
+              className="absolute right-[67.0%] top-[62.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
               style={inoutDigitalValueStyle}
             >
               <span className="inline-flex w-full items-end justify-end gap-[0.12em]">
@@ -2065,7 +2065,7 @@ function LegacyThermostatPanel({
               </span>
             </div>
             <div
-              className="absolute right-[10.0%] top-[36.0%] flex h-[18.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
+              className="absolute right-[18.0%] top-[36.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
               style={inoutDigitalValueStyle}
             >
               <span className="inline-flex w-full items-end justify-end gap-[0.12em]">
@@ -2076,7 +2076,7 @@ function LegacyThermostatPanel({
               </span>
             </div>
             <div
-              className="absolute right-[10.0%] top-[62.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
+              className="absolute right-[18.0%] top-[62.0%] flex h-[12.0%] w-[20.0%] items-center justify-end pr-[6%] text-right text-red-600"
               style={inoutDigitalValueStyle}
             >
               <span className="inline-flex w-full items-end justify-end gap-[0.12em]">
@@ -2087,7 +2087,7 @@ function LegacyThermostatPanel({
               </span>
             </div>
 
-            <div className="absolute left-[28.6%] top-[31.7%] h-[44.7%] w-[29.8%] overflow-hidden rounded-[14px] border border-white/20 bg-black/60 p-1.5">
+            <div className="absolute left-[28.0%] top-[31.7%] h-[44.7%] w-[29.8%] overflow-hidden rounded-[14px] border border-white/20 bg-black/60 p-1.5">
               <img
                 src={selectedPhoto.imageSrc}
                 alt={`Imagen seleccionada: ${selectedPhoto.label}`}
