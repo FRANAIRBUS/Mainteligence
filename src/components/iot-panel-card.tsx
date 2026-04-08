0f799a448755e67fc51361d67cff203690d56142
Fran <francb.movil@gmail.com>
2026-04-08 02:00:08 +0100

    Actualiza posiciones panel

--- src/components/iot-panel-card.tsx
+++ src/components/iot-panel-card.tsx
@@ -2440,7 +2440,7 @@ function LegacyThermostatPanel({
             >
               <div className="flex items-center gap-2">
                 <Send className="h-4 w-4 text-sky-300" />
-                <span>Provision Code</span>
+                <span>Provision</span>
               </div>
             </button>
           )}
